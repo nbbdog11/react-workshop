e07c4e593f6f2dd842caa31821518c83c7565916
Scott Wagner <scottywags16@gmail.com>
2017-09-27 17:53:47 -0400

    dynamically generate table from JSON data

--- subjects/JSONTable/exercise.js
+++ subjects/JSONTable/exercise.js
@@ -9,10 +9,81 @@ import 'purecss/build/pure.css'
 import React from 'react'
 import { render } from 'react-dom'
 import getJSON from './lib/getJSON'
+import PropTypes from 'prop-types'
+
+function isURL(content) {
+  return (/^https?:\/\//).test(content)
+}
+
+function isImageURL(content) {
+  return isURL(content) && (/\.(jpe?g|gif|png)$/).test(content)
+}
 
 const JSONTable = React.createClass({
+  propTypes: {
+    src: PropTypes.string.isRequired,
+    getData: PropTypes.func.isRequired
+  },
+
+  getInitialState() {
+    return {
+      data: null
+    }
+  },
+
+  componentDidMount() {
+    getJSON(this.props.src, (err, payload) => {
+      this.setState({
+        data: this.props.getData(payload)
+      })
+    })
+  },
+
+  formatContent(content) {
+    if (Array.isArray(content))
+      return content.map(this.formatContent)
+
+    if (isImageURL(content))
+      return <p><img key={content} height="64" src={content}/></p>
+
+    if (isURL(content))
+      return <p><a key={content} href={content}>{content}</a></p>
+
+    return content
+  },
+
+  getTableHeaders(fields) {
+    return fields.map(field => (<th key={field}>{field}</th>))
+  },
+
+  buildRow(fields, data) {
+    return (
+      <tr key={data.id}>
+        {fields.map(field => (<td key={field}>{this.formatContent(data[field])}</td>))}
+      </tr>
+    )
+  },
+
   render() {
-    return <div>...</div>
+    const { data } = this.state
+
+    if( data == null || data.length === 0 )
+      return null
+
+    const fields = Object.keys(data[0])
+
+    return (
+      <table className="pure-table pure-table-striped">
+        <thead>
+          <tr>
+            {this.getTableHeaders(fields)}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item => this.buildRow(fields, item))}
+        </tbody>
+      </table>
+    )
   }
 })
 
